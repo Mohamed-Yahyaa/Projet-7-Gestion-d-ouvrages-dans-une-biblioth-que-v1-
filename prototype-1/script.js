bb2c35f0933601d4e2ee5fc90449e6de79848a87
mohamedyahya <mohamed.yahya.dev@gmail.com>
2021-12-09 09:59:27 +0100

    up

--- prototype-1/script.js
+++ prototype-1/script.js
@@ -12,7 +12,7 @@ function readwork() {
     work["Auteur"] = document.getElementById("inputAuteur").value;
     work["Prix"] = parseFloat(document.getElementById("inputPrix").value);
     work["date"] = document.getElementById("inputDate").value;
-    work["language"] = document.getElementById("inputLanguage").value;
+    work["langue"] = document.getElementById("inputLangue").value;
     var cheackValues = document.getElementsByName("workType");
     for (var i = 0; i < cheackValues.length; i++) {
         if (cheackValues[i].checked) {
@@ -28,11 +28,11 @@ function readwork() {
 function insertNewRow(work) {
     var tableBody = document.getElementById("worksTable").getElementsByTagName('tbody')[0];
     var newRow = tableBody.insertRow(tableBody.length);
-    newRow.insertCell(0).innerHTML = work.title;
+    newRow.insertCell(0).innerHTML = work.Titre;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = work.author;
+    cell2.innerHTML = work.Auteur;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = work.price;
+    cell3.innerHTML = work.Prix;
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = work.date;
     cell5 = newRow.insertCell(4);
